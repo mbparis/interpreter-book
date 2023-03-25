688e3837c5896dc8a4467dd0d0e5aab7c16913d5
mbparis <briceparis@gmail.com>
2023-03-25 16:24:49 -0500

    add token set

--- token/token.go
+++ token/token.go
@@ -7,6 +7,32 @@ type Token struct {
 	Literal string
 }
 
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
 func Hello() string {
 	return "world"
 }
