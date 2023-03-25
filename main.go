4e4f8e2a35100a276adf9f0318ab375f2e735ff3
mbparis <briceparis@gmail.com>
2023-03-25 16:20:05 -0500

    hello world

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mbparis/interpreter-book/token"
+)
+
+func main() {
+	fmt.Printf("hello %v \n", token.Hello())
+
+}
---
+++ token/token.go
@@ -0,0 +1,12 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+func Hello() string {
+	return "world"
+}
