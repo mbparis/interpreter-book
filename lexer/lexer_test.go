5f52d22d973d8ef56f5b543459c58c64d3fdbadc
mbparis <briceparis@gmail.com>
2023-05-20 14:53:24 -0500

    additional tokens

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -93,4 +93,78 @@ var _ = Describe("Lexer", func() {
 		)
 
 	})
+	Describe("will iterate over source code; not necessarily syntax corect", Ordered, func() {
+		input := `
+		let five = 5;
+		let ten = 10;
+
+		let add = fn(x, y) {
+			x + y;
+		};
+
+		let result = add(five, ten);
+		!-/*5;
+		5 < 10 > 5;
+		`
+		sut := lexer.New(input)
+
+		DescribeTable("outputting source code as tokens",
+			func(tokenType token.TokenType, tokenLiteral string) {
+				actual := sut.NextToken()
+				fmt.Printf("__actual: %+v", actual)
+				Expect(actual.Type).To(Equal(tokenType))
+				Expect(actual.Literal).To(Equal(tokenLiteral))
+			},
+			Entry("when LET", token.TokenType(token.LET), "let"),
+			Entry("when IDENT", token.TokenType(token.IDENT), "five"),
+			Entry("when ASSIGN", token.TokenType(token.ASSIGN), "="),
+			Entry("when INT", token.TokenType(token.INT), "5"),
+			Entry("when SEMICOLON", token.TokenType(token.SEMICOLON), ";"),
+			Entry("when LET", token.TokenType(token.LET), "let"),
+			Entry("when IDENT", token.TokenType(token.IDENT), "ten"),
+			Entry("when ASSIGN", token.TokenType(token.ASSIGN), "="),
+			Entry("when INT", token.TokenType(token.INT), "10"),
+			Entry("when SEMICOLON", token.TokenType(token.SEMICOLON), ";"),
+			Entry("when LET", token.TokenType(token.LET), "let"),
+			Entry("when IDENT", token.TokenType(token.IDENT), "add"),
+			Entry("when ASSIGN", token.TokenType(token.ASSIGN), "="),
+			Entry("when FUNCTION", token.TokenType(token.FUNCTION), "fn"),
+			Entry("when LPAREN", token.TokenType(token.LPAREN), "("),
+			Entry("when IDENT", token.TokenType(token.IDENT), "x"),
+			Entry("when COMMA", token.TokenType(token.COMMA), ","),
+			Entry("when IDENT", token.TokenType(token.IDENT), "y"),
+			Entry("when RPAREN", token.TokenType(token.RPAREN), ")"),
+			Entry("when LBRACE", token.TokenType(token.LBRACE), "{"),
+			Entry("when IDENT", token.TokenType(token.IDENT), "x"),
+			Entry("when PLUS", token.TokenType(token.PLUS), "+"),
+			Entry("when IDENT", token.TokenType(token.IDENT), "y"), //
+			Entry("when SEMICOLON", token.TokenType(token.SEMICOLON), ";"),
+			Entry("when RBRACE", token.TokenType(token.RBRACE), "}"),
+			Entry("when SEMICOLON", token.TokenType(token.SEMICOLON), ";"),
+			Entry("when LET", token.TokenType(token.LET), "let"),
+			Entry("when IDENT", token.TokenType(token.IDENT), "result"),
+			Entry("when ASSIGN", token.TokenType(token.ASSIGN), "="),
+			Entry("when IDENT", token.TokenType(token.IDENT), "add"),
+			Entry("when LPAREN", token.TokenType(token.LPAREN), "("),
+			Entry("when IDENT", token.TokenType(token.IDENT), "five"),
+			Entry("when COMMA", token.TokenType(token.COMMA), ","),
+			Entry("when IDENT", token.TokenType(token.IDENT), "ten"),
+			Entry("when RPAREN", token.TokenType(token.RPAREN), ")"),
+			Entry("when SEMICOLON", token.TokenType(token.SEMICOLON), ";"),
+			Entry("when BANG", token.TokenType(token.BANG), "!"),
+			Entry("when MINUS", token.TokenType(token.MINUS), "-"),
+			Entry("when SLASH", token.TokenType(token.SLASH), "/"),
+			Entry("when ASTERISK", token.TokenType(token.ASTERISK), "*"),
+			Entry("when INT", token.TokenType(token.INT), "5"),
+			Entry("when SEMICOLON", token.TokenType(token.SEMICOLON), ";"),
+			Entry("when INT", token.TokenType(token.INT), "5"),
+			Entry("when LT", token.TokenType(token.LT), "<"),
+			Entry("when INT", token.TokenType(token.INT), "10"),
+			Entry("when GT", token.TokenType(token.GT), ">"),
+			Entry("when INT", token.TokenType(token.INT), "5"),
+			Entry("when SEMICOLON", token.TokenType(token.SEMICOLON), ";"),
+			Entry("when EOF", token.TokenType(token.EOF), ""),
+		)
+
+	})
 })
